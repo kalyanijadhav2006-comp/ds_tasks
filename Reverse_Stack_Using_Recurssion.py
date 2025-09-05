f55db22ba8d625869faeb2fa17e14473688bcfbc
kalyanijadhav2006-comp <kalyanijadhav2006@gmail.com>
2025-09-05 10:50:39 +0530

    Add files via upload

---
+++ Reverse_Stack_Using_Recurssion.py
@@ -0,0 +1,25 @@
+def insert_sorted(stack, element):
+    
+    if not stack or stack[-1] <= element:
+        stack.append(element)
+        return
+    
+    top = stack.pop()
+    insert_sorted(stack, element)
+    stack.append(top)
+
+def sort_stack(stack):
+    
+    if not stack:
+        return
+    
+    top = stack.pop()
+    
+    sort_stack(stack)
+    
+    insert_sorted(stack, top)
+
+
+stack = [3, 1, 4, 2]
+sort_stack(stack)
+print(stack)  
